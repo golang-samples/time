0589df6d8e3b34c99edb593184a9c42e9ca99377
Ato ARAKI <ato.araki@gmail.com>
2013-02-20 19:52:26 +0900

    Add sample of format function

---
+++ format/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// You can use Format argument of http://golang.org/pkg/time/#pkg-constants :
+	fmt.Println(time.Now().Format(time.Kitchen))
+
+	// also, you can input directly as string :
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Println(time.Now().Format("15:04:05"))
+}
