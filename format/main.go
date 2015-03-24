e3d42c016e5448212c0a44a62dc0572ef1d8e8c5
Nguyen Dang Minh <nguyendangminh@users.noreply.github.com>
2015-03-24 23:12:05 +0700

    Update main.go

--- format/main.go
+++ format/main.go
@@ -12,4 +12,7 @@ func main() {
 	// also, you can input directly as string :
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println(time.Now().Format("15:04:05"))
+	
+	// Timestamp
+	fmt.Println(time.Now().Unix()) // Ex: 1257894000
 }
